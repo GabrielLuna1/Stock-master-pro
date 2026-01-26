4fdf9b154ec9b2a45fa53a8998d69b516c71ced5
Gabriel Luna <gabriellunajob@gmail.com>
2026-01-26 15:23:57 -0300

    Chore: Configuracao de Variaveis de Ambiente para Auto-Update de Versao

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // 👇 ADICIONE ESTE BLOCO 'env':
+  env: {
+    // Isso pega a variável secreta da Vercel (backend)
+    // e "tatua" ela no código do Frontend para o navegador ler.
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
+  },
 };
 
 export default nextConfig;
